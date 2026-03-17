731f3532c9c1f56873477de039942a3e92dffde1
Haris Waheed <ah6086395@gmail.com>
2026-03-17 23:04:16 +0500

    Database Connection Created!

--- database.py
+++ database.py
@@ -4,4 +4,20 @@ from dotenv import load_dotenv
 
 env_file = load_dotenv()
 
+USER = os.environ.get('USER')
+PASSWORD = os.environ.get('PASSWORD')
+DSN = 'localhost/XEPDB1'
 
+
+def get_db():
+
+    connection = None
+
+    try:
+        connection = oracledb.connect(user=USER, password=PASSWORD, dsn=DSN)
+        print('Database Connected!')
+        yield connection
+
+    finally:
+        if connection:
+            connection.close()
