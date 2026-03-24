299b4824f771a5ef2d4e702ef648abb20a88123f
Haris Waheed <ah6086395@gmail.com>
2026-03-24 23:48:40 +0500

    added verify product procedure and product quantity in table

---
+++ Db_Script/Orders.sql
@@ -0,0 +1,89 @@
+
+DECLARE order_count NUMBER;
+
+BEGIN
+    SELECT COUNT(*) INTO order_count FROM USER_TABLES
+        WHERE TABLE_NAME = 'ORDERS';
+
+    IF order_count = 0
+        THEN
+
+        EXECUTE IMMEDIATE 'CREATE TABLE ORDERS(
+        order_id NUMBER GENERATED ALWAYS AS IDENTITY (NOCACHE) PRIMARY KEY ,
+        customer_name VARCHAR2(40) NOT NULL ,
+        customer_number VARCHAR2(13) NOT NULL ,
+        customer_city VARCHAR2(100) NOT NULL ,
+        customer_address VARCHAR2(200) NOT NULL ,
+        customer_bill NUMBER NOT NULL ,
+        order_date DATE DEFAULT TRUNC(SYSDATE),
+        quantity NUMBER NOT NULL ,
+        product_id NUMBER NOT NULL ,
+        FOREIGN KEY (product_id) REFERENCES INVENTORY(product_id)
+    )';
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('TABLE ORDERS ALREADY EXISTS!');
+
+    END IF;
+END;
+
+CREATE OR REPLACE PROCEDURE p_add_order(
+
+    p_name IN VARCHAR2,
+    p_number IN VARCHAR2,
+    p_city IN VARCHAR2,
+    p_address IN VARCHAR2,
+    p_bill IN NUMBER,
+    p_product_id IN NUMBER,
+    p_quantity IN NUMBER,
+    p_order_id OUT NUMBER
+)
+AS BEGIN
+    INSERT INTO ORDERS(customer_name, customer_number, customer_city, customer_address, customer_bill,
+                       quantity, PRODUCT_ID)
+    VALUES (p_name, p_number,
+            p_city, p_address, p_bill,
+            p_quantity, p_product_id)
+            RETURNING ORDER_ID INTO p_order_id;
+
+    COMMIT ;
+END;
+
+CREATE OR REPLACE PROCEDURE p_display_orders(
+
+    ref_cur OUT SYS_REFCURSOR
+)
+AS BEGIN
+    OPEN ref_cur FOR
+        SELECT CUSTOMER_NAME, CUSTOMER_NUMBER, CUSTOMER_CITY, CUSTOMER_ADDRESS, CUSTOMER_BILL,
+               TO_CHAR(ORDER_DATE, 'DD-MM_YYYY'), PRODUCT_ID, ORDER_ID, QUANTITY
+            FROM ORDERS;
+
+END;
+
+CREATE OR REPLACE PROCEDURE p_display_order(p_order_id IN VARCHAR2)
+    AS
+    p_order SYS_REFCURSOR;
+
+    BEGIN
+    OPEN p_order FOR
+        SELECT CUSTOMER_NAME, CUSTOMER_NUMBER, CUSTOMER_CITY, CUSTOMER_ADDRESS, CUSTOMER_BILL,
+               TO_CHAR(ORDER_DATE, 'DD-MM_YYYY'), PRODUCT_ID, ORDER_ID, QUANTITY
+            FROM ORDERS
+                WHERE ORDER_ID = p_order_id;
+
+    DBMS_SQL.RETURN_RESULT(p_order);
+
+end;
+
+SELECT * FROM ORDERS;
+
+CREATE OR REPLACE PROCEDURE p_verify_product(p_product_id IN NUMBER)
+AS
+    result_cur SYS_REFCURSOR;
+BEGIN
+    OPEN result_cur FOR
+        SELECT PRODUCT_ID FROM INVENTORY
+            WHERE PRODUCT_ID = p_product_id;
+
+   DBMS_SQL.RETURN_RESULT(result_cur);
+end;
