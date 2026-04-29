b3f7749bcd82dad9b2456a6e3c04ba4b68b24a7f
Haris Waheed <ah6086395@gmail.com>
2026-04-29 17:09:14 +0500

    Added product_id to return in post endpoint and added update_product endpoint.

--- Routers/inventory.py
+++ Routers/inventory.py
@@ -6,14 +6,18 @@ from database import get_db
 
 router = APIRouter(prefix='/inventory', tags=['INVENTORY'])
 
+
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def add_inventory(new_product: InventoryIn, db= Depends(get_db)):
 
     try:
         with db.cursor() as cursor:
             product_link = str(new_product.product_link) if new_product.product_link else None
+            product_id = cursor.var(int)
 
-            cursor.callproc('p_add_inventory', [new_product.product_name,
+            cursor.callproc('p_add_inventory', [
+                                            product_id,
+                                            new_product.product_name,
                                             new_product.product_description,
                                             new_product.product_wholesale,
                                             new_product.product_retail,
@@ -21,15 +25,14 @@ def add_inventory(new_product: InventoryIn, db= Depends(get_db)):
                                             product_link,
                                             new_product.product_category
                                              ])
-        result = {
+        return {
+            'product_id' : product_id.getvalue(),
             'product_name' : new_product.product_name,
             'product_retail' : new_product.product_retail,
             'product_image' : new_product.product_image,
             'product_category' : new_product.product_category,
             'product_link' : new_product.product_link
         }
-
-        return result
 
     except oracledb.Error as e:
         print(f'Error Regarding DataBase: {e}')
@@ -75,6 +78,7 @@ def display_customer_inventory(db= Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail='Failed to load inventory data. Please try again later.')
 
+
 @router.get('/admin', response_model=list[InventoryAdminOut])
 def display_admin_inventory(db= Depends(get_db)):
 
@@ -115,6 +119,7 @@ def display_admin_inventory(db= Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail='Failed to load inventory data. Please try again later.')
 
+
 @router.get('/{item_id}', response_model=InventoryCustomerOut)
 def display_product(item_id: int, db=Depends(get_db)):
 
@@ -150,6 +155,7 @@ def display_product(item_id: int, db=Depends(get_db)):
 
     return result
 
+
 @router.delete('/{product_id}')
 def delete_product(product_id : int, db = Depends(get_db)):
 
@@ -175,6 +181,7 @@ def delete_product(product_id : int, db = Depends(get_db)):
        print('Database Error', e)
        raise HTTPException(status_code=500, detail='Error Occurred On Our Side!')
 
+
 @router.patch('/{product_id}/status')
 def update_product_status(product_id : int, new_status : ProductStatus, db = Depends(get_db)):
 
@@ -194,4 +201,45 @@ def update_product_status(product_id : int, new_status : ProductStatus, db = Dep
 
     except oracledb.Error as e:
         print('Error For Updating Status', e)
+        raise HTTPException(status_code=500, detail='Error Occurred On Our Side!')
+
+
+@router.put('/{product_id}')
+def update_product(product_id : int, updated_product : InventoryIn, db=Depends(get_db)):
+
+    try:
+        product_link = str(updated_product.product_link) if updated_product.product_link else None
+
+        with db.cursor() as cursor:
+            cursor.callproc('p_verify_product', [product_id])
+            feedback = cursor.getimplicitresults()
+            actual_fb = feedback[0].fetchone() if feedback else None
+
+            if actual_fb:
+
+                cursor.callproc('p_edit_product', [ product_id,
+                                                    updated_product.product_name,
+                                                    updated_product.product_description,
+                                                    updated_product.product_wholesale,
+                                                    updated_product.product_retail,
+                                                    updated_product.product_image,
+                                                    product_link,
+                                                    updated_product.product_category
+                                                    ])
+
+                return {
+                    'product_name': updated_product.product_name,
+                    'product_description' : updated_product.product_description,
+                    'product_wholesale' : updated_product.product_wholesale,
+                    'product_retail': updated_product.product_retail,
+                    'product_image': updated_product.product_image,
+                    'product_link': product_link,
+                    'product_category': updated_product.product_category
+                }
+
+            else:
+                raise HTTPException(status_code=404, detail=f'Product {product_id} Not Exists!')
+
+    except oracledb.Error as e:
+        print('Error For Updating Product', e)
         raise HTTPException(status_code=500, detail='Error Occurred On Our Side!')
