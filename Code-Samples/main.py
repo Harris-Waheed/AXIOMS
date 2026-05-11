29e3d2566cb14352e754047e1ed744012daf13f9
Haris Waheed <ah6086395@gmail.com>
2026-05-11 22:22:50 +0500

    Added main.py
    Set up FastAPI with CORS middleware and include routers.

--- Code-Samples/main.py
+++ Code-Samples/main.py
@@ -1,1 +1,29 @@
+from fastapi import FastAPI
+from Routers import checkout, inventory, reviews, login
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://axioms-pk.me",
+    "https://www.axioms-pk.me"
+]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_headers=['*'],
+                   allow_credentials=False,
+                   allow_methods=['*'],
+                   )
+
+app.include_router(login.router)
+app.include_router(checkout.router)
+app.include_router(inventory.router)
+app.include_router(reviews.router)
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='localhost', port=8000)
